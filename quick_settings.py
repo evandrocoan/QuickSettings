25567cb1ca213f80edda9f9276a23147085b4434
evandrocoan <evandrocoan@hotmail.com>
2018-01-13 14:54:39 -0200

    Renamed the debug_tools.py to logger.py

--- quick_settings.py
+++ quick_settings.py
@@ -15,7 +15,7 @@ import ast
 import json
 
 # # Import the debugger
-# from python_debug_tools.debug_tools import getLogger
+# from python_debug_tools import getLogger
 
 # # Enable debug messages: (bitwise)
 # #
